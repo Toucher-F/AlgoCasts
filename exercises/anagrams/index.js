2ba0ba7cd4dd290d657609d47721057ddf4b2af1
Toucher-F <lianduanb@gmail.com>
2018-02-14 16:21:41 -0500

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,79 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   let charMapA = {};
+//   let charMapB = {};
+//   let check = true;
+//   const lowerA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//   const lowerB = stringB.replace(/[^\w]/g, "").toLowerCase();
+//   for (let A of lowerA) {
+//     if (!charMapA[A]) {
+//       charMapA[A] = 1;
+//     } else {
+//       charMapA[A]++;
+//     }
+//   }
+//   for (let B of lowerB) {
+//     if (!charMapB[B]) {
+//       charMapB[B] = 1;
+//     } else {
+//       charMapB[B]++;
+//     }
+//   }
+//
+//   for (let C of lowerA) {
+//     if (charMapA[C] != charMapB[C]) {
+//       check = false;
+//       break;
+//     }
+//   }
+//   for (let D of lowerB) {
+//     if (charMapA[D] != charMapB[D]) {
+//       check = false;
+//       break;
+//     }
+//   }
+//   return check;
+// }
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] != bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//
+// function buildCharMap(str) {
+//   const charMap = {};
+//
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//
+//   return charMap;
+// }
