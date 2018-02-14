8c0c7fd6fe36d70c8c58c5c0d465699bf466ae74
Toucher-F <lianduanb@gmail.com>
2018-02-13 19:32:12 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+  for (let c of str) {
+    if (!charMap[c]) {
+      charMap[c] = 1;
+    } else {
+      charMap[c]++;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
