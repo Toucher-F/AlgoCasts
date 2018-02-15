bf5aa6ae02ae7228644b9f61940698bd4c1f6f51
Toucher-F <lianduanb@gmail.com>
2018-02-15 11:17:06 -0500

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,50 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let stepArr = [];
+//     for (let j = 1; j <= i; j++) {
+//       stepArr.push("#");
+//     }
+//     for (let k = 1; k <= n - i; k++) {
+//       stepArr.push(" ");
+//     }
+//     console.log(stepArr.join(""));
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
