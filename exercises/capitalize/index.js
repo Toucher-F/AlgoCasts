c79541ca9d022e6b1521b2bf31998b0c723021a8
Toucher-F <lianduanb@gmail.com>
2018-02-15 10:28:51 -0500

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,39 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   let arr = str.split("");
+//
+//   arr.splice(0, 1, arr[0].toUpperCase());
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === " " && arr[i + 1]) {
+//       arr.splice(i + 1, 1, arr[i + 1].toUpperCase());
+//     }
+//   }
+//   return arr.join("");
+// }
+
+// function capitalize(str) {
+//   const words = [];
+//
+//   for (let word of str.split(" ")) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(" ");
+// }
+
