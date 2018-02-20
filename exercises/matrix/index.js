ed6b5f061cfc1c6368e4797cae443fe222d908c3
Toucher-F <lianduanb@gmail.com>
2018-02-20 16:45:49 -0500

    matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,120 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top endRow
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // Right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    //  left column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
 
 module.exports = matrix;
+
+// function changeDirection(dir) {
+//   switch (dir) {
+//     case "right":
+//       return "down";
+//     case "down":
+//       return "left";
+//     case "left":
+//       return "up";
+//     case "up":
+//       return "right";
+//   }
+// }
+//
+// function findNextPoint(dir, row, column) {
+//   let point = { row, column };
+//   switch (dir) {
+//     case "right":
+//       point.column += 1;
+//       break;
+//     case "down":
+//       point.row += 1;
+//       break;
+//     case "left":
+//       point.column -= 1;
+//       break;
+//     case "up":
+//       point.row -= 1;
+//       break;
+//   }
+//   return point;
+// }
+//
+// function matrix(n) {
+//   let arr = [];
+//   for (let i = 0; i < n; i++) {
+//     arr.push([]);
+//     for (let j = 0; j < n; j++) {
+//       arr[i].push(0);
+//     }
+//   }
+//
+//   // let arr = new Array(n);
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   arr[i] = new Array(n);
+//   // }
+//   let row = 0;
+//   let column = 0;
+//   let dir = "right";
+//   for (let count = 1; count <= n * n; count++) {
+//     //console.log(dir);
+//
+//     arr[row][column] = count;
+//
+//     //console.log(arr);
+//     if (
+//       findNextPoint(dir, row, column).row >= n ||
+//       findNextPoint(dir, row, column).column >= n
+//     ) {
+//       dir = changeDirection(dir);
+//     } else if (
+//       arr[findNextPoint(dir, row, column).row][
+//         findNextPoint(dir, row, column).column
+//       ] != 0
+//     ) {
+//       dir = changeDirection(dir);
+//     }
+//     //console.log(findNextPoint(dir, row, column));
+//     row = findNextPoint(dir, row, column).row;
+//     column = findNextPoint(dir, row, column).column;
+//     //debugger;
+//   }
+//   return arr;
+// }
