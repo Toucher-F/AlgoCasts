e7712b5ced760178dec7374b309aaef5f8b51f74
Toucher-F <lianduanb@gmail.com>
2018-02-28 11:45:01 -0500

    bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,58 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  // insert(data) {
+  //   const newNode = new Node(data);
+  //   let node = this;
+  //   while (1) {
+  //     if (newNode.data < node.data) {
+  //       if (node.left === null) {
+  //         node.left = newNode;
+  //         return newNode;
+  //       }
+  //       node = node.left;
+  //     } else {
+  //       if (node.right === null) {
+  //         node.right = newNode;
+  //         return newNode;
+  //       }
+  //       node = node.right;
+  //     }
+  //   }
+  // }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (data === this.data) {
+      return this;
+    } else if (data < this.data && this.left) {
+      return this.left.contains(data);
+    } else if (data < this.left) {
+      return null;
+    } else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    } else if (data > this.data) {
+      return null;
+    }
+  }
+}
 
 module.exports = Node;
